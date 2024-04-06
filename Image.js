fecd25a5d1741fb581c629516a42492388fe3478
app1 <saieesh.soma1919@gmail.com>
2024-04-06 12:25:44 +0530

    Updated reset,verify and added some CSS

--- Image.js
+++ Image.js
@@ -1,42 +1,68 @@
-let images=document.querySelectorAll("img");
-let resetBtn=document.querySelector("#reset");
-let verifyBtn=document.querySelector("#verify");
-let output=document.querySelector("#output")
-console.log(resetBtn)
-resetBtn.style.display="none";
-verifyBtn.style.display="none";
-let clickedTimes=0;
-let clickArray=[];
+let images = document.querySelectorAll("img");
+let resetBtn = document.querySelector("#reset");
+let verifyBtn = document.querySelector("#verify");
+let output = document.querySelector("#output")
+let sixthimg = document.querySelector("#image");
 
-images.forEach((elem,index)=>{
-    elem.addEventListener("click",()=>{
-        clickedTimes=clickedTimes+1;
-        if (clickedTimes==2) 
-        {   console.log(index);
-            clickArray.push(elem)
-            resetBtn.style.display="none";
-            verifyBtn.style.display="";
-        } 
-        if (clickedTimes==1) 
-        {   clickArray.push(elem)
-            console.log(index)
-            resetBtn.style.display="";
+resetBtn.style.display = "none";
+verifyBtn.style.display = "none";
+
+let clickedTimes = 0;
+let clickArray = [];
+
+let ran = Math.floor(Math.random() * 5);
+sixthimg.innerHTML = `<img src=${images[ran].src}>`;
+images = document.querySelectorAll("img");
+
+images.forEach((elem, index) => {
+    elem.addEventListener("click", () => {
+        elem.style.border="2px solid black";
+        clickedTimes = clickedTimes + 1;
+        if (clickedTimes == 2) {
+            console.log(index);
+            clickArray.push(elem.src)
+            resetBtn.style.display = "none";
+            verifyBtn.style.display = "";
+            console.log(clickArray)
         }
-        if (clickedTimes>2) {
+        if (clickedTimes == 1) {
+            clickArray.push(elem.src)
+            console.log(index)
+            resetBtn.style.display = "";
+        }
+        if (clickedTimes > 2) {
             alert("You Can only select 2 images");
-            window.location.reload();
+            images.forEach(img => {
+                img.style.border = "solid 2px red";
+            });
+            output.innerHTML = "<h2>Please Reset and Click only 2 Images to verify</h2>"
+            resetBtn.style.display = "";
+            verifyBtn.style.display = "none";
+
         }
     })
 })
+
+function reset() {
+    resetBtn.style.display = "none";
+    clickedTimes = 0;
+    clickArray = [];
+    images.forEach(img => {
+        img.style.border = "";
+    });
+    output.innerHTML = "";
+}
+
 console.log(clickArray)
-function check(){
-    let str=``;
-    if(clickArray[0].className==clickArray[1].className){
-        alert("Verified!!");
-       str=str+"You are a human. Congratulations!";
+
+function check() {
+    let str = ``;
+    if (clickArray[0] == clickArray[1]) {
+
+        str = str + "<h2 style='color:darkgreen;text-shadow: 5px 5px 10px #00FF00;'>You are a human. Congratulations!</h2>";
     }
-    else{
-        str=str+"We can't verify you as a human."
+    else {
+        str = str + "<h2 style='color:red'>We can't verify you as a human.</h2>"
     }
-    output.innerHTML=str;
+    output.innerHTML = str+`<span style='font-size:50px'>&#128175;</span>`;
 }
